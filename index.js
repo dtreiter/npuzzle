7d0fcc72d805851b20042d768eb74978040445de
Daniel Reiter <semibmx@gmail.com>
2026-07-26 20:55:17 -0700

    Remove obsolete .gitignore

---
+++ index.js
@@ -0,0 +1,9 @@
+import {App} from './app.js';
+
+// Use `location.hash` (i.e. #) routing convention.
+m.route.mode = 'hash';
+
+// Render controls.
+m.route(document.getElementById('controls'), '/', {
+	'/': App
+});
