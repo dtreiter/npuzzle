1d5214ee0cf33ac7dfa39e6b6354d53e82fafcce
Daniel Reiter <semibmx@gmail.com>
2026-08-04 17:51:23 -0700

    Add node build script
    This unblocks using TypeScript in the future.

---
+++ build.mjs
@@ -0,0 +1,68 @@
+import * as esbuild from 'esbuild';
+import {
+  access,
+  copyFile,
+  cp,
+  mkdir,
+  rm,
+} from 'node:fs/promises';
+
+const isWatch = process.argv.includes('--watch');
+
+async function buildJsCss() {
+  if (isWatch) {
+    // Use the context API for rebuilding on file save
+    const js = await esbuild.context({
+      entryPoints: ['src/js/index.js'],
+      outfile: 'dist/bundle.js',
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+    });
+    const css = await esbuild.context({
+      entryPoints: ['src/css/index.css'],
+      outfile: 'dist/bundle.css',
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+    });
+
+    await js.watch();
+    await css.watch();
+    console.log('Watching for changes...');
+  } else {
+    const js = esbuild.build({
+      entryPoints: ['src/js/index.js'],
+      outfile: 'dist/bundle.js',
+      bundle: true,
+      minify: true,
+      sourcemap: true,
+    });
+    const css = esbuild.build({
+      entryPoints: ['src/css/index.css'],
+      outfile: 'dist/bundle.css',
+      bundle: true,
+      minify: true,
+      sourcemap: true,
+    });
+    await Promise.all([js, css]);
+    console.log('Build complete');
+  }
+}
+
+async function build() {
+  // Remove previous build if it exists.
+  try {
+    await access('dist');
+    await rm('dist', {recursive: true});
+  } catch {}
+
+  await mkdir('dist');
+  await mkdir('dist/deps');
+  await copyFile('src/index.html', 'dist/index.html');
+  await cp('src/deps', 'dist/deps', {recursive: true});
+
+  await buildJsCss();
+}
+
+await build();
